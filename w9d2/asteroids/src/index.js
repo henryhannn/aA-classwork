ac0247219bed126cca13b74ee27fc769d2e54046
henryhannn <henryhan3996@gmail.com>
2020-07-28 12:12:19 -0700

    initial commit

---
+++ w9d2/asteroids/src/index.js
@@ -0,0 +1,9 @@
+const MovingObject = require("./moving_object.js");
+
+window.MovingObject = MovingObject;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const canvas1 = document.getElementById("game-canvas");
+
+    const ctx = canvas1.getContext("2d");
+});
