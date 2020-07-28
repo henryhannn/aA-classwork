5fb920a2def8519e404a205b95dc8cbedf5a198f
henryhannn <henryhan3996@gmail.com>
2020-07-28 12:16:32 -0700

    added inherits to util.js

--- w9d2/asteroids/src/util.js
+++ w9d2/asteroids/src/util.js
@@ -1,1 +1,8 @@
-Function
+const Util = {
+    inherits(childClass, parentClass) {
+        childclass.prototype = Object.create(parentClass.prototype);
+        childClass.prototype.constructor = childClass;
+    }
+};
+  
+module.exports = Util;
